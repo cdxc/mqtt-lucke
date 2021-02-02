56e96bd72127e5dd39824795a5baa9da9e040b6e
CDXC <afna@krivic.net>
2021-02-02 22:04:56 +0100

    made it smaller

--- switch.py
+++ switch.py
@@ -1,24 +1,8 @@
-#!/usr/bin/python3.9
+#!/usr/bin/python3.7
 
 import paho.mqtt.client as mqtt
 import time
-
-# Prebere podatke za prijavo iz "auth" datoteke
-a = open('auth', 'r')
-content = a.read()
-u,p,x = content.split("\n")
-
-
-# Preveri povezavo
-def on_connect(client, userdata, flags, rc):
-        print("Povezava je izpostavljena: "+str(rc))
-
-
-# Defines
-c = mqtt.Client()
-c.username_pw_set(username=u,password=p)
-c.on_connect=on_connect
-
-# Prizge 
-c.connect("192.168.1.10", port=1883)
-c.publish("cmnd/room_2svitlight/POWER", payload='TOGGLE')
+import mqttlib 
+mqttlib.profile()
+mqttlib.connect()
+mqttlib.switch()
