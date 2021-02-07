76cbb9d713b5d2289fffc4e7347bd6444f249a73
CDXC <afna@krivic.net>
2021-02-07 19:41:19 +0100

    added voice control

--- voice-control.py
+++ voice-control.py
@@ -1,16 +1,27 @@
 import speech_recognition as sr
+import mqttlib
 
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+sr.Microphone.list_microphone_names()
+mqttlib.identify()
+mqttlib.connect()
+
+#for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
 	print("Speak your word:")
 	audio = r.listen(source)
-	try:
-		text = r.ibm(audio)
-		print('You said: {}'.format(text))
-	except:
-		print('not working')
+	r.adjust_for_ambient_noise(source)
+	r.energy_threshold = 4000 
+	r.dynamic_energy_threshold = True
 
+try:
+	text = r.recognize_google(audio)
+	print('You said: {}'.format(text))
+	if("light" in text):
+		mqttlib.light_toggle("TOGGLE")
+
+except:
+	print('not working')
